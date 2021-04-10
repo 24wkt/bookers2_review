741419236f37692d83c3a60f2ec5f93aa69bcbec
24wkt <05wkt38xy.waku@gmail.com>
2021-04-10 14:32:42 +0000

    [add]user/index&show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      flash[:notice] = "Book was successfully created"
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -28,6 +29,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated"
       redirect_to book_path
     else
       render 'edit'
@@ -37,6 +39,7 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
+    flash[:notice] = "Book was successfully destroyed"
     redirect_to books_path
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,21 @@
 class UsersController < ApplicationController
   def index
+    @user = current_user
+    @users = User.all
+    @book = Book.new
   end
-  
+
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books.all
   end
-  
+
   def edit
+    @user = User.find(params[:id])
   end
-  
+
   def update
   end
-  
+
 end
