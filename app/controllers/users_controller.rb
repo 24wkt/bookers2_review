867ea9e1f4fbd5fa7633f970c560edc2b1db61ab
24wkt <05wkt38xy.waku@gmail.com>
2021-04-11 04:26:18 +0000

    [add]直打ち対策

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_action :ensure_correct_user, only: [:edit, :update]
+
   def index
     @user = current_user
     @users = User.all
@@ -23,7 +26,18 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
 
+ private
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,7 @@ ActiveRecord::Schema.define(version: 2021_04_10_053018) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: falseexit
-    
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
     t.text "introduction"
